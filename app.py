a0af6dca85e4135ab6e6952307dff0e44c139f71
Srishti <srishtimini14@gmail.com>
2026-02-24 21:47:50 +0530

    Added Feature: Basic UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, render_template
+from flask import Flask, render_template, url_for
 import requests
 
 app = Flask(__name__)
@@ -6,21 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # header | side bar | description| styling | projects - category | live link | updates - currently | contact me
-    return render_template('index.html')
+    return render_template('index.html', active='home')
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', active='projects')
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", active='contact')
 
 @app.route('/footer')
 def footer():
-    return render_template_string("""
-                                 <footer>Contact me: <a href="mailto:
-        <p>&copy; 2024 Your Name. All rights reserved.</p>""")
+    return render_template('footer.html') 
     
 if __name__ == '__main__':
     app.run(debug=True)    
