b0b901e07b8b0069c1fab246c794b783ba496a47
Srishti <srishtimini14@gmail.com>
2026-04-10 23:15:30 +0530

    update : adding project card structure

--- static/script.js
+++ static/script.js
@@ -16,3 +16,39 @@ document.addEventListener('DOMContentLoaded', () => {
     heading.appendChild(span);
   });
 });
+
+
+let curr = 0;
+const slides = document.getElementById('slides');
+const totalSlides = slides.children.length;
+const dotContainer = document.getElementById('dots');
+// Created dots
+for(let i =0; i<totalSlides; i++){
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if(i===0) dot.classList.add('active');
+    dot.addEventListener('click', () => goTo(i));
+    dotContainer.appendChild(dot);
+}
+
+// dots movement updated
+function updateDots(){
+    document.querySelectorAll('.dot').forEach((d, i) => {
+       d.classList.toggle('active', i === curr);
+    });
+}
+
+//parsing 
+function goTo(index) {
+   curr = (index + totalSlides) % totalSlides;
+   slides.style.transform = `translateX(-${curr * 100}%)`;
+   updateDots();
+}
+
+//changing of slides
+function changeSlide(dir) {
+  goTo(curr + dir);
+}
+
+// auto play
+setInterval (() => changeSlide(1), 3000);
